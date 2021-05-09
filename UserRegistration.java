ce237a69f0aad256a829aad34824fca7bbacce63
prudhvi <prudhvieethakota123@gmail.com>
2021-05-09 19:20:55 +0530

    Added:Code for Validating the Email Address in Regex

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,10 @@
 package com.userregistration;
 
 import java.util.Scanner;
-
+/**
+ * @description create class for Defining the User Registration program
+ *
+ */
 public class UserRegistration {
     //constant
     private static final Scanner sc = new Scanner(System.in);
@@ -20,6 +23,22 @@ public class UserRegistration {
             System.out.println("Name Not Valid. Try using Conditions");
         }
     }
+    /**
+     * @description create method for Email Address
+     *
+     */
+    private void eMailAddress () {
+        System.out.println("Enter the Email Address");
+        String eMailAddress = sc.nextLine();
+        String regex = "^[a-z0-9]+(([.+-_][a-z0-9])?)+(@[a-z0-9]{1})+(.[a-z]{3,4})+((.[a-z]{2})?)$";//Regex Pattern
+        //Matching the given eMailAddress with regular expression
+        boolean eMail = eMailAddress.matches(regex);
+        if (eMail) { //Verifying whether given eMailAddress is valid
+            System.out.println("Valid EMail Address");
+        } else {
+            System.out.println("EMail Address Not Valid. Try using Conditions");
+        }
+    }
     //Explaining how to Implement the Computation
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
@@ -29,5 +48,7 @@ public class UserRegistration {
         System.out.println("Enter the Last Name");
         UserRegistration lastName = new UserRegistration(); //creating lastname object
         lastName.nameValidation();
+        UserRegistration eMailAddress = new UserRegistration(); //creating eMailAddress object
+        eMailAddress.eMailAddress();
     }
 }
