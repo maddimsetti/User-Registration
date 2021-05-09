270c517f8a0122e4ca3522925741b4e155761dc2
prudhvi <prudhvieethakota123@gmail.com>
2021-05-09 19:01:36 +0530

    Added:Code for Validating First Name in Regex

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,30 @@
 package com.userregistration;
 
+import java.util.Scanner;
+
 public class UserRegistration {
+    //constant
+    private static final Scanner sc = new Scanner(System.in);
+    /**
+     * @description create method for name Validation
+     *
+     */
+    private void nameValidation () {
+        String name = sc.nextLine();
+        String regex = "^[A-Z][a-zA-Z]{3,}$";   //Regex Pattern
+        //Matching the given name with regular expression
+        boolean result = name.matches(regex);
+        if (result) { //Verifying whether given name is valid
+            System.out.println("Valid Name");
+        } else {
+            System.out.println("Name Not Valid. Try using Conditions");
+        }
+    }
+    //Explaining how to Implement the Computation
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
+        System.out.println("Enter the First Name");
+        UserRegistration firstName = new UserRegistration(); //creating firstname object
+        firstName.nameValidation();
     }
 }
